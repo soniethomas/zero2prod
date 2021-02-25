58bded5a73bd8dbd55bdb75629486b60f429c56c
Sonie Thomas <soniethomas.dev@gmail.com>
2021-02-24 20:11:07 -0500

    Chapter 3.0

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use zero2prod::run;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("[fe80::9d27:3ea:17de:2516%9]:8000")?;
+    run(listener)?.await
 }
